b98489a448f49b8b14daf0fdf0caf4947aa184c0
janbrin <jan.brinkkemper@vng.nl>
2025-06-13 14:16:32 +0200

    a

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ let respecConfig = {
   specType: "im",
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
-  specStatus: "IO",
+  specStatus: "CV",
   publishDate: "2025-06-01",
   publishVersion: "1.2.0",
 
